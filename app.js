ec9b872855eb67710f3929e4d7349883cd22e64f
Dineshkumar-mighty <dineshkumar@mightyapps.ai>
2025-04-22 17:27:16 +0530

    App.js: Changes in Dynamic Message

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const name = "Dineshkumar Ramaraj";
+
 app.get("/", (req, res) => {
-  res.send("Hello World From DineshkumarRamaraj!");
+  const message = `Hello World From ${name}!`;
+  res.send(message);
 });
 
 const server = app.listen(3000, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-module.exports = { app, server };
+module.exports = { app, server, name };
