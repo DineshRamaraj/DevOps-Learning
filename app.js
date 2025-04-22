e0c588dc39359fe0e89abaad5fceb1bedd25b5c0
Dineshkumar-mighty <dineshkumar@mightyapps.ai>
2025-04-22 17:20:57 +0530

    App.js: Changes in Message

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World From Dineshkumar!");
+  res.send("Hello World From DineshkumarRamaraj!");
 });
 
 const server = app.listen(3000, () => {
