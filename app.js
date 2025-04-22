ba61eb647708d4972ba2c0d60a4e4e1e1d5b69ea
Dineshkumar-mighty <dineshkumar@mightyapps.ai>
2025-04-22 17:10:27 +0530

    Add CI Pipeline with Github Actions

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.get("/", (req, res) => {
   res.send("Hello World From Dineshkumar!");
 });
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = { app, server };
